d5c96b9cfff581de2ffdeb752be51959166ad6ff
BILLA <adithyen1@gmail.com>
2026-02-05 16:04:46 +0530

    this is a error handling verswion of the http and read file

--- node_core/server.js
+++ node_core/server.js
@@ -1,12 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 http.createServer((req, res) => {
-    if(req.url === "/"){
-        fs.readFile("data.txt","utf8",(err, data)=>{
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.end(data);
-        });
-    }
+  if (req.url === "/") {
+
+    const filePath = path.join(__dirname, "data.txt");
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("File not found or server error");
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(data);
+    });
+  }
 }).listen(3000);
-    console.log("Server is Running......");          
+
+console.log("Server running on http://localhost:3000");
